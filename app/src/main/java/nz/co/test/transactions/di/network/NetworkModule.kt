c07ae9ba7b0ad3a5a4eba77ab5938ed49adf7096
Tiger <tigerlilyh777@gmail.com>
2025-03-08 15:29:08 +1300

    Implement domain layer and viewmodel

--- app/src/main/java/nz/co/test/transactions/di/network/NetworkModule.kt
+++ app/src/main/java/nz/co/test/transactions/di/network/NetworkModule.kt
@@ -1,11 +1,16 @@
 package nz.co.test.transactions.di.network
 
 import com.squareup.moshi.Moshi
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import nz.co.test.transactions.data.repository.TransactionListRepository
+import nz.co.test.transactions.data.repository.TransactionListRepositoryImpl
 import nz.co.test.transactions.data.services.TransactionsService
+import nz.co.test.transactions.domain.usecase.TransactionListUseCase
+import nz.co.test.transactions.domain.usecase.TransactionListUseCaseImpl
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -50,4 +55,17 @@ class NetworkModule {
     @Singleton
     fun providesTransactionService(retrofit: Retrofit): TransactionsService =
         retrofit.create(TransactionsService::class.java)
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    abstract class TransactionBindings {
+
+        @Singleton
+        @Binds
+        abstract fun bindTransactionUseCase(impl: TransactionListUseCaseImpl): TransactionListUseCase
+
+        @Singleton
+        @Binds
+        abstract fun bindTransactionRepository(impl: TransactionListRepositoryImpl): TransactionListRepository
+    }
 }
